e9beb9f22a3ee03f71e30a9c75d67d399b5bd5b0
truongnq <quangtruong0799@gmail.com>
2022-12-08 17:29:50 +0700

    modal: create baseModal for displaying mockup

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import BannerReducer from '../slices/banner';
+import  ShowroomReduce  from '../slices/showroom';
 const store = configureStore({
     reducer: {
         banner: BannerReducer,
+        showroom:ShowroomReduce
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
