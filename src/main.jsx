78612c0b6623c5d968d5bdabf106d57fdc46470f
truongnq <quangtruong0799@gmail.com>
2022-12-08 17:29:50 +0700

    modal: create baseModal for displaying mockup

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,15 @@ import vi_VN from 'antd/locale/vi_VN';
 import App from './App';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import { ModalProvider } from './contexts/ModalContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
         <ConfigProvider locale={vi_VN}>
             <BrowserRouter>
-                <App />
+                <ModalProvider>
+                    <App />
+                </ModalProvider>
             </BrowserRouter>
         </ConfigProvider>
     </Provider>,
