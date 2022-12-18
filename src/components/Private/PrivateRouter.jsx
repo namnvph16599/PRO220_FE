88528b450dc61e33fb6a80f31ccd353c7e692088
Ngô Vưn Nam <anh02284@gmail.com>
2022-12-18 11:35:59 +0700

    fix: private router

--- src/components/Private/PrivateRouter.jsx
+++ src/components/Private/PrivateRouter.jsx
@@ -1,11 +1,8 @@
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { Token } from '../../constants/auth';
 
 const PrivateRouter = ({ children }) => {
-    const navigate = useNavigate();
-    if (localStorage.getItem(Token.accessToken)) {
-        navigate('/');
-    }
+    if (localStorage.getItem(Token.accessToken)) return <Navigate to="/" />;
     return children;
 };
 
