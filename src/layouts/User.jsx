b2834f9aa0f28994d5f39079ac76cea89dca007b
= <huynguyenz2002@gmail.com>
2023-01-31 22:32:00 +0700

    fix: login

--- src/layouts/User.jsx
+++ src/layouts/User.jsx
@@ -4,24 +4,24 @@ import { Dropdown, Space } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
 import { logout, saveUserValues } from '../slices/user';
 import { JwtDecode } from '../utils/auth';
-import { Token } from '../constants/auth';
+import { ROLE, Token } from '../constants/auth';
+import jwtDecode from 'jwt-decode';
+import { isEmpty } from 'lodash';
 
 const User = (props) => {
     const items = [];
     const [isAdmin, setIsAdmin] = useState(false);
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user.currentUser.values);
+    const accessToken = useSelector((state) => state.user.currentUser.accessToken);
     const isLogged = useSelector((state) => state.user.isLogged);
 
     useEffect(() => {
         const userDecode = JwtDecode();
-        if (userDecode) {
-            dispatch(saveUserValues(userDecode));
-            if (!userDecode.role) {
-                setIsAdmin(false);
-            } else {
-                setIsAdmin(true);
-            }
+        if (userDecode || !isEmpty(accessToken)) {
+            const Jwt = userDecode ? userDecode : jwtDecode(accessToken);
+            dispatch(saveUserValues(Jwt));
+            [ROLE.ADMIN, ROLE.MANAGER].includes(Jwt.role) ? setIsAdmin(true) : setIsAdmin(false);
         }
     }, [isLogged]);
     const hanldelogout = () => {
