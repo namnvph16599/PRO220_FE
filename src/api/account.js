60bc93eeabcae9df5044516a1454412a514fd972
= <huynguyenz2002@gmail.com>
2023-02-06 10:58:29 +0700

    feat:Update User

--- src/api/account.js
+++ src/api/account.js
@@ -1,1 +1,12 @@
+import _ from 'lodash';
 import instance from './instance';
+
+const URL = '/accounts';
+
+export const updateAccount = async (data) => {
+    return instance.put(`/accounts/${data._id}`, _.omit(data, ['_id']));
+};
+
+export const getUser = async (id) => {
+    return instance.get(`/account/${id}`);
+};
--- src/slices/user.js
+++ src/slices/user.js
@@ -1,11 +1,21 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { login } from '../api/auth';
 import jwtDecode from 'jwt-decode';
+import { updateAccount } from '../api/account';
+import _ from 'lodash';
 
 export const loginAsync = createAsyncThunk('user/login', async (values, { rejectWithValue }) => {
     try {
         const { data } = await login(values);
         return data;
+    } catch (error) {
+        return rejectWithValue(error);
+    }
+});
+export const UpdateUser = createAsyncThunk('user/Updater', async (values, { rejectWithValue }) => {
+    try {
+        const { data } = await updateAccount(values);
+        return values;
     } catch (error) {
         return rejectWithValue(error);
     }
@@ -16,6 +26,9 @@ const initialState = {
     currentUser: {
         values: {},
         accessToken: '',
+    },
+    UpdateUser: {
+        status: '',
     },
     isLogged: false,
     error: '',
@@ -48,11 +61,20 @@ export const userSlice = createSlice({
             if (action.payload.message) {
                 state.error = action.payload.message;
             } else {
-                state.currentUser.values = jwtDecode(action.payload.accessToken);
+                state.currentUser.values = _.omit(jwtDecode(action.payload.accessToken), [
+                    'showroomId',
+                    'role',
+                    'iat',
+                    'exp',
+                ]);
                 state.currentUser.accessToken = action.payload.accessToken;
                 state.error = '';
                 state.isLogged = true;
             }
+        },
+        [UpdateUser.fulfilled.type]: (state, action) => {
+            state.currentUser.values = state.currentUser.values._id == action.payload._id && action.payload;
+            state.UpdateUser.status = 'sussecc';
         },
     },
 });
